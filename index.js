9ea7be01758f45cabf9754a4a9201d5c305d5f4e
Zach <zmommaerts@gmail.com>
2022-01-09 14:49:05 -0600

    stuck

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const createEmployeeRecord = (array) => {
-    array.firstName = array[0];
+const createEmployeeRecord = (row) => {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (array) => {
+    return array.forEach(row => {
+        createEmployeeRecord(row)
+    });
 }
